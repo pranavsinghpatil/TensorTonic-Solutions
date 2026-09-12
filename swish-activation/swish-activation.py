9d4f09714b852e34cdcbb14138474c4b0a5949d3
Pranav <102906940+pranavsinghpatil@users.noreply.github.com>
2026-09-13 00:23:14 +0530

    Solve swish-activation

---
+++ swish-activation/swish-activation.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def swish(x: list) -> np.ndarray:
+    """
+    Returns a NumPy array with the same shape as x.
+    """
+    # Write code here
+    x = np.asarray(x, dtype=float)
+
+    sig = np.exp(-np.logaddexp(0.0, -x)) 
+
+    return x * sig
